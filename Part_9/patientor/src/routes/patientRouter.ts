7fd1e2aa08a4b27dc4d66bd162189c7724e911a3
Yaups <tpallison.ta@gmail.com>
2024-02-22 14:05:30 +0000

    Exercise 9.12 completed

--- Part_9/patientor/src/routes/patientRouter.ts
+++ Part_9/patientor/src/routes/patientRouter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import patientService from '../services/patientService';
 
@@ -5,6 +6,26 @@ const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.json(patientService.getAllNonSensitivePatients());
+});
+
+router.post('/', (req, res) => {
+  console.log('Patient request received');
+
+  //get object from request body
+  const { name, dateOfBirth, gender, ssn, occupation } = req.body;
+
+  //eventually perform validation on this input
+
+  //add this patient to list of patients
+  const newNonSensitivePatient = patientService.addNewPatient({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  });
+
+  res.json(newNonSensitivePatient);
 });
 
 export default router;
--- Part_9/patientor/src/services/patientService.ts
+++ Part_9/patientor/src/services/patientService.ts
@@ -1,6 +1,6 @@
 import patients from '../../data/patients';
-
-import { NonSensitivePatient } from '../types';
+import { NonSensitivePatient, NewPatient, Patient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getAllNonSensitivePatients = (): NonSensitivePatient[] => {
   return patients.map((patient) => ({
@@ -12,4 +12,40 @@ const getAllNonSensitivePatients = (): NonSensitivePatient[] => {
   }));
 };
 
-export default { getAllNonSensitivePatients };
+const addNewPatient = (newPatient: NewPatient): NonSensitivePatient => {
+  console.log('New patient added!', newPatient);
+
+  console.log('Patients then:', patients);
+
+  //add id field to patient
+
+  /*
+  const spoof: Patient = {
+    id: uuid(),
+    name: 'spoof spooferson',
+    dateOfBirth: '1990-01-01',
+    ssn: '234234',
+    gender: 'male',
+    occupation: 'spoof job',
+  };
+  */
+
+  const spoof: Patient = {
+    id: uuid(),
+    ...newPatient,
+  };
+
+  patients.push(spoof);
+
+  console.log('Patients now:', patients);
+
+  return {
+    id: spoof.id,
+    name: spoof.name,
+    dateOfBirth: spoof.dateOfBirth,
+    gender: spoof.gender,
+    occupation: spoof.occupation,
+  };
+};
+
+export default { getAllNonSensitivePatients, addNewPatient };
